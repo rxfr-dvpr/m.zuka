225fa5d4ee9c6fd8b4e32755c2737f133c6fea4c
Avazbek <saidaliyev.avazbek14@gmail.com>
2023-09-16 15:14:28 +0500

    domain configuration changed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/m.zuka/',
+  base: '/m-zuka-kids.netlify.app/',
   plugins: [
     vue(),
   ],
